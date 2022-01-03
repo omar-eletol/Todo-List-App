0ee8fce8388acf341e405321c1a2a99c97e86362
omar eletol <omareletol@gmail.com>
2022-01-03 14:45:14 +0200

    Test If there is one completed task and no active tasks

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -19,4 +19,23 @@ class StatisticsUtilsTest {
         assertEquals(result.completedTasksPercent, 0f)
         assertEquals(result.activeTasksPercent, 100f)
     }
+
+    //If there is one completed task and no active tasks,
+    // the activeTasks percentage should be 0f,
+    // the completed tasks percentage should be 100f.
+    @Test
+    fun getActiveAndCompletedStats_oneCompleted_returnsHundredZero() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = true)
+
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 100f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
 }
