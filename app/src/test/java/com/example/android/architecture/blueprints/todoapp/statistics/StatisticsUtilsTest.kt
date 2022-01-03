899b915ccfc2c07e1157010f9ce5ea6e733ecc6a
omar eletol <omareletol@gmail.com>
2022-01-03 15:15:48 +0200

    Test If there is an empty list (emptyList())

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -60,4 +60,35 @@ class StatisticsUtilsTest {
         assertEquals(result.completedTasksPercent, 40f)
         assertEquals(result.activeTasksPercent, 60f)
     }
+
+
+    // If there is an empty list (emptyList()), then both percentages should be 0f.
+    //If there was an error loading tasks, the list will be null, then both percentages should be 0f.
+    @Test
+    fun getActiveAndCompletedStats_emptyList_returnsZeros() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = emptyList<Task>()
+
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_nullList_returnsZeros() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = null
+
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
 }
