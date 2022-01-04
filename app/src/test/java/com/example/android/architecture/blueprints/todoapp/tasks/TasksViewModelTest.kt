bfee4d638d522f886da1715d8ad8066951f15208
omar eletol <omareletol@gmail.com>
2022-01-04 20:21:46 +0200

    finish (addNewTaskEventLiveData) test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -11,17 +17,22 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
 
         // Given a fresh TasksViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-
         // When adding a new task
-
+        tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
-
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(
+                value.getContentIfNotHandled(), (not(nullValue()))
+        )
     }
 }
